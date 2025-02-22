438efb7a7af826c57b5fae8cfcc646ecddf2528d
quynhdeptrai306 <nguyentrucquynh3006@gmail.com>
2025-02-22 10:28:00 +0700

    feat(models): add some models

---
+++ lib/database/models/transaction.model.ts
@@ -0,0 +1,32 @@
+import { Schema, model, models } from 'mongoose';
+
+const TransactionSchema = new Schema({
+  stripeId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  plan: {
+    type: String,
+  },
+  credits: {
+    type: Number,
+  },
+  buyer: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Transaction =
+  models?.Transaction || model('Transaction', TransactionSchema);
+
+export default Transaction;
