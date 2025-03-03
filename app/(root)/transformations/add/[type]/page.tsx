2cedb739bc41a09fc6aaac661462e0fea7d12dc4
quynhdeptrai306 <nguyentrucquynh3006@gmail.com>
2025-03-03 22:28:32 +0700

    add header

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,7 +1,15 @@
+import Header from '@/components/shared/Header';
 import React from 'react';
-
-const AddTransformationTypePage = () => {
-  return <div>AddTransformationPage</div>;
+import { transformationTypes } from '@/constants';
+import TransformationForm from '@/components/shared/TransformationForm';
+const AddTransformationTypePage = ({ params: { type } }: SearchParamProps) => {
+  const transformation = transformationTypes[type];
+  return (
+    <>
+      <Header title={transformation.title} subtitle={transformation.subTitle} />
+      <TransformationForm/>
+    </>
+  );
 };
 
 export default AddTransformationTypePage;
